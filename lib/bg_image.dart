4ad27ecc81475ae94eaf347858295e693c38c02b
aanchal-1995 <aanchalmalhotra1512@gmail.com>
2020-09-09 16:40:25 +0530

    Login Page

--- lib/bg_image.dart
+++ lib/bg_image.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class BGImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/BG.jpeg");
+    return Image.asset(
+      "assets/BG.jpeg",
+      fit: BoxFit.cover,
+      color: Colors.black.withOpacity(0.7),
+      colorBlendMode: BlendMode.colorDodge,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+// import 'dart:js';
+
+import 'package:awesome_app/Pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'Pages/home_page.dart';
+// import 'Pages/home_page.dart';
+import 'Pages/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "AWESOME APP",
-    home: HomePage(),
+    home: LoginPage(),
     theme: ThemeData(primarySwatch: Colors.amber),
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/home": (context) => HomePage()
+    },
   ));
 }
